f2203821751f29d521d8a0bddc7c9bbd60108518
Niccolla <nlosacco@ymail.com>
2018-05-22 09:14:28 +0200

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls')),
+    
     url(r'^$', views.post_list, name='post_list'),
 ]
